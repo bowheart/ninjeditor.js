52190138ecc6010a4548ddcd91cf6bc870cc0c7d
bowheart <kingdomdance602@gmail.com>
2016-10-28 08:07:09 -0600

    code cleanup

--- src/ninjeditor.js
+++ src/ninjeditor.js
@@ -3,80 +3,211 @@
 	
 	
 	(function(outside) {
-		var weapons = {
-			'h1': {text: 'h1'},
-			'h2': {text: 'h2'},
-			'bold': {icon: 'bold'},
-			'italic': {icon: 'italic'},
-			'underline': {icon: 'underline'},
-			'strikethrough': {icon: 'strikethrough'},
-			'text-left': {icon: 'align-left'},
-			'text-center': {icon: 'align-center'},
-			'text-right': {icon: 'align-right'},
-			'text-justify': {icon: 'align-justify'}
-		}
+		var El = function(tagName, attributes, content) {
+			tagName || (tagName = 'div');
+			var classes = tagName.split('.');
+			tagName = classes.shift();
+			var el = this._el = document.createElement(tagName || 'div');
+			
+			if (classes.length) {
+				this.addClass(classes.join(' '));
+			}
+			
+			if (typeof attributes === 'object' && !Array.isArray(attributes)) {
+				Object.keys(attributes).forEach(function(attr) {
+					el.setAttribute(attr, attributes[attr]);
+				});
+			}
+			
+			if (content) this.html(content);
+		};
+		El.prototype = {
+			addClass: function(theClass) {
+				this.el.className = this.el.className.split(' ').concat(theClass.split(' ')).join(' ');
+				return this;
+			},
+			append: function(el) {
+				if (el instanceof El) el = el.el;
+				this.el.appendChild(el);
+				return this;
+			},
+			attr: function(key, val) {
+				if (typeof val === 'undefined') return this.el[key];
+				this.el.setAttribute(key, val);
+				return this;
+			},
+			hide: function() {
+				this.el.hidden = true;
+			},
+			html: function(val) {
+				if (typeof val === 'undefined') return this.el.innerHTML;
+				this.el.innerHTML = val;
+				return this;
+			},
+			on: function(eventName, handler) {
+				this.el.addEventListener(eventName, handler);
+			},
+			show: function() {
+				this.el.removeAttribute('hidden');
+			},
+			toggle: function() {
+				this[this.el.hidden ? 'show' : 'hide']();
+			},
+			
+			get el() { return this._el; }
+		};
 		
-		var getContent = function(weapon) {
-			weapon = weapons[weapon];
-			return weapon.icon ? '<i class="fa fa-' + weapon.icon + '"></i>' : weapon.text;
-		}
 		
-		
+		// Expose part of this submodule
+		outside.El = El;
+	})(inside);
+	
+	
+	(function(outside) {
 		var weaponPrototype = {
+			get button() {
+				return this._icon ? '<i class="fa fa-' + this._icon + '"></i>' : this._text;
+			}
+		};
+		var weapons = {
+			'h1': {
+				_text: 'h1'
+			},
+			'h2': {
+				_text: 'h2'
+			},
+			'bold': {
+				_icon: 'bold'
+			},
+			'italic': {
+				_icon: 'italic'
+			},
+			'underline': {
+				_icon: 'underline'
+			},
+			'strikethrough': {
+				_icon: 'strikethrough'
+			},
+			'text-left': {
+				_icon: 'align-left'
+			},
+			'text-center': {
+				_icon: 'align-center'
+			},
+			'text-right': {
+				_icon: 'align-right'
+			},
+			'text-justify': {
+				_icon: 'align-justify'
+			}
+		};
+		
+		var weaponConstructors = {};
+		
+		/*
+			The weaponFactory learns as the program is used.
+			The individual weapon prototypes are defined in the 'weapons' object, but they aren't turned into actual 'classes' until they're needed.
+			Then they're stored for later use.
+		*/
+		var weaponFactory = function(weapon) {
+			if (weaponConstructors[weapon]) return new weaponConstructors[weapon]();
+			
+			weapon = weapons[weapon];
+			var Weapon = new Function();
+			Weapon.prototype = weapon;
+			Object.setPrototypeOf(Weapon.prototype, weaponPrototype);
+			weaponConstructors[weapon] = Weapon;
+			return new Weapon();
+		};
+		
+		
+		// Expose part of this submodule
+		outside.weaponFactory = weaponFactory;
+	})(inside);
+	
+	
+	(function(outside) {
+		var toolPrototype = {
 			get el() { return this._el; }
 		};
 		
 		
 		var Group = function(config) {
-			var el = this._el = document.createElement('div');
-			el.className = 'ninjeditor-group';
+			var el = this._el = new outside.El();
+			el.addClass('ninja-group');
 			
 			for (var i = 0; i < config.children.length; i++) {
 				var child = config.children[i],
-					button = document.createElement('button');
+					button = new outside.El('button');
 				
-				button.innerHTML = getContent(child);
-				button.className = 'ninjeditor-btn';
-				el.appendChild(button);
+				button.html(outside.weaponFactory(child));
+				button.addClass('ninja-btn');
+				el.append(button);
 			}
 		};
-		Group.prototype = weaponPrototype;
+		Group.prototype = toolPrototype;
 		
 		
 		var Dropdown = function(config) {
-			var el = this._el = document.createElement('div'),
-				button = document.createElement('button')
-				dropdown = document.createElement('ul');
-			
-			el.className = 'ninjeditor-group';
-			button.className = 'ninjeditor-btn';
-			button.innerHTML = config.icon ? '<i class="fa fa-' + config.icon + '"></i>' : config.text;
-			button.innerHTML += ' <i class="fa fa-caret-down"></i>';
-			dropdown.className = 'ninjeditor-dropdown';
-			
-			for (var i = 0; i < config.children.length; i++) {
-				var child = config.children[i],
-					option = document.createElement('li');
-				
-				option.value = child;
-				option.innerHTML = getContent(child);
-				dropdown.appendChild(option);
-			}
-			el.appendChild(button);
-			el.appendChild(dropdown);
+			this.initEl(config);
 		};
-		Dropdown.prototype = weaponPrototype;
+		Dropdown.prototype = {
+			initEl: function(config) {
+				var el = this._el = new outside.El(),
+					button = new outside.El('button'),
+					dropdown = new outside.El('ul');
+				
+				el.addClass('ninja-group');
+				
+				var buttonHtml = config.icon ? '<i class="fa fa-' + config.icon + '"></i>' : config.text;
+				buttonHtml += ' <i class="fa fa-caret-down"></i>';
+				button.addClass('ninja-btn');
+				button.html(buttonHtml);
+				
+				dropdown.addClass('ninja-dropdown');
+				dropdown.hide();
+				
+				for (var i = 0; i < config.children.length; i++) {
+					var child = config.children[i],
+						option = new outside.El('li');
+					
+					option.attr('value', child);
+					option.html(outside.weaponFactory(child));
+					dropdown.append(option);
+				}
+				el.append(button).append(dropdown);
+				this.bind(button, dropdown);
+			},
+			bind: function(button, dropdown) {
+				button.on('click', this.handleButtonClick.bind(this, button, dropdown));
+			},
+			handleButtonClick: function(button, dropdown, event) {
+				dropdown.toggle();
+				if (!dropdown.attr('hidden')) { // we just showed the dropdown.
+					var causeClose = function() {
+						document.removeEventListener('click', causeClose);
+						document.addEventListener('click', close);
+					};
+					var close = function() {
+						document.removeEventListener('click', close);
+						dropdown.hide();
+					}
+					document.addEventListener('click', causeClose);
+				}
+			}
+		};
+		Object.setPrototypeOf(Dropdown.prototype, toolPrototype);
 		
 		
 		var Single = function(config) {
-			var el = this._el = document.createElement('button');
-			el.innerHTML = getContent(config.content);
-			el.className = 'ninjeditor-group ninjeditor-btn';
+			var el = this._el = new outside.El('button');
+			el.html(outside.weaponFactory(config.content));
+			el.addClass('ninja-group ninja-btn');
 		};
-		Single.prototype = weaponPrototype;
+		Single.prototype = toolPrototype;
 		
 		
-		var weaponFactory = {
+		var toolFactory = {
 			new: function(config) {
 				if (typeof config === 'string') config = {type: 'single', content: config};
 				
@@ -86,27 +217,28 @@
 					case 'single': return new Single(config);
 				}
 			}
-		}
+		};
 		
 		
 		// Expose part of this submodule
-		outside.weaponFactory = weaponFactory;
+		outside.toolFactory = toolFactory;
 	})(inside);
 	
 	
 	(function(outside) {
 		var Toolbar = function(embedded, config) {
 			this.embedded = embedded;
-			var el = this._el = document.createElement('div');
-			el.className = 'ninjeditor-toolbar';
+			var el = this._el = new outside.El();
+			el.addClass('ninja-toolbar');
 			
 			this.construct(el, config);
 		};
 		Toolbar.prototype = {
-			construct: function(el, weapons) {
-				for (var i = 0; i < weapons.length; i++) {
-					var weapon = outside.weaponFactory.new(weapons[i]);
-					el.appendChild(weapon.el);
+			construct: function(el, tools) {
+				for (var i = 0; i < tools.length; i++) {
+					var tool = outside.toolFactory.new(tools[i]);
+					el.append(tool.el);
+					if (i < tools.length - 1) el.append(new outside.El('span.ninja-divider'));
 				}
 			},
 			get el() { return this._el; }
@@ -141,13 +273,13 @@
 						{type: 'dropdown', icon: 'bold', children: ['bold', 'italic']},
 						{type: 'group', children: ['h1', 'h2']},
 					];
-				} else if (config === 'normal') { // TODO: actually implement this interface
+				} else if (config === 'normal') { // TODO: actually implement this template
 					config = [
 						{type: 'group', children: ['text-left', 'text-center']},
 						{type: 'dropdown', icon: 'bold', children: ['bold', 'italic']},
 						{type: 'group', children: ['h1', 'h2']},
 					];
-				} else if (config === 'full') { // TODO: actually implement this interface
+				} else if (config === 'full') { // TODO: actually implement this template
 					config = [
 						{type: 'group', children: ['text-left', 'text-center']},
 						{type: 'dropdown', icon: 'bold', children: ['bold', 'italic']},
@@ -186,32 +318,29 @@
 		};
 		Ninjeditor.prototype = {
 			initEl: function(config) {
-				var el = this._el = document.createElement('div'),
+				var el = this._el = new outside.El('div', config.attrs),
 					editor = this._editor = el;
 				
 				if (config.type !== 'inline') { // stick the toolbar and the editor in there
-					editor = this._editor = document.createElement('div');
-					el.appendChild(this.toolbar.el);
-					el.appendChild(editor);
+					editor = this._editor = new outside.El();
+					el.addClass('ninja');
+					el.append(this.toolbar.el);
+					el.append(editor);
 				}
 				
-				editor.contentEditable = true;
-
-				if (typeof config.attrs === 'object') {
-					Object.keys(config.attrs).forEach(function(attr) {
-						el.setAttribute(attr, config.attrs[attr]);
-					});
-				}
-				if (config.val) editor.innerHTML = config.val;
+				editor.addClass('ninja-editor');
+				editor.attr('contentEditable', true);
+				if (config.val) editor.html(config.val);
+				
 				this.bind(editor);
 			},
 			
 			bind: function(el) {
-				el.onkeydown = this.handleKeydown.bind(this, el);
-				el.oninput = this.handleInput.bind(this, el);
+				el.on('keydown', this.handleKeydown.bind(this, el));
+				el.on('input', this.handleInput.bind(this, el));
 				if (this.config.type === 'inline') {
-					el.onfocus = this.handleFocus.bind(this, el);
-					el.onblur = this.handleBlur.bind(this, el);
+					el.on('focus', this.handleFocus.bind(this, el));
+					el.on('blur', this.handleBlur.bind(this, el));
 				}
 			},
 			
@@ -261,7 +390,7 @@
 			
 			get config() { return this._config; },
 			get editor() { return this._editor; },
-			get el() { return this._el; },
+			get el() { return this._el.el; },
 			get val() { return this.el.innerHTML; }
 		};
 		
